2e8526a2be33c4a978a21c0e3e84d71200045d60
Pravesh Kunwar <praveshisawesome2017@gmail.com>
2020-09-28 18:01:41 -0400

    Added mongodb for database purposes.

--- Events/ready.js
+++ Events/ready.js
@@ -1,4 +1,18 @@
+const { GridFSBucket } = require("mongodb");
+
 module.exports = (bot) => {
+  const MongoClient = require("mongodb").MongoClient;
+  const { url } = require("../Config/config");
+  MongoClient.connect(
+    url,
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    (err, db) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(`Connected to Database.`);
+    }
+  );
   console.log(`Logged in as ${bot.user.tag}`);
   bot.user
     .setPresence({
